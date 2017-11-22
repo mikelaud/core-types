aebf02c4a5350fe0020e50609098d0033806a4d1
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2017-11-22 03:14:14 +0300

    error style

--- src/main/java/mikelaud/core/types/FxStyle.java
+++ src/main/java/mikelaud/core/types/FxStyle.java
@@ -3,9 +3,15 @@ package mikelaud.core.types;
 public interface FxStyle {
 
 	String READ_ONLY =
-		"-fx-background-insets: 0 1 1 0, 1 0 0 1, 1; " +
-		"-fx-background-radius: 3px; " +
-		"-fx-border-radius: 3px; " +
-		"-fx-background-color: rgba(0, 0, 0, 0.75), rgba(255, 255, 255, 0.75), linear-gradient(to bottom, lightgrey 0%, gainsboro 100%); ";
-	
+			"-fx-background-insets: 0 1 1 0, 1 0 0 1, 1; " +
+			"-fx-background-radius: 3px; " +
+			"-fx-border-radius: 3px; " +
+			"-fx-background-color: rgba(0, 0, 0, 0.75), rgba(255, 255, 255, 0.75), linear-gradient(to bottom, lightgrey 0%, gainsboro 100%); ";
+		
+	String READ_ONLY_ERROR =
+			"-fx-background-insets: 0 1 1 0, 1 0 0 1, 1; " +
+			"-fx-background-radius: 3px; " +
+			"-fx-border-radius: 3px; " +
+			"-fx-background-color: rgba(0, 0, 0, 0.75), rgba(255, 255, 255, 0.75), linear-gradient(to bottom, pink 0%, gainsboro 100%); ";
+		
 }
